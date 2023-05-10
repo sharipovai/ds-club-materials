0a0084c2a49fae946497215fb36ccb15a6c5fc71
timofann <anytat@inbox.ru>
2023-05-11 01:20:45 +0300

    env settings

---
+++ setupvenv.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "${BASH_SOURCE-}" = "$0" ]; then
+    echo "You must source this script: source $0" >&2
+    exit 33
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+
+CONDAVENV="$SCRIPT_DIR/condavenv"
+
+
+
+echo "Check for update environment $CONDAVENV\0" # виртуальное окружение над основным интерпретатором
+#
+if [ ! -d $CONDAVENV ]; then
+    conda create -y python=3.7 pip --prefix $CONDAVENV; fi
+if ! echo $PATH | grep --silent ^$CONDAVENV/bin; then
+    conda activate --stack $CONDAVENV; fi
+pip install jupyter numpy pandas matplotlib seaborn jupyterlab ipykernel jupyter-lsp jupyterlab-lsp python-language-server jedi==0.17.2;
+ipython kernel install --name "ds-club-kernel" --user;
+jupyter lab
